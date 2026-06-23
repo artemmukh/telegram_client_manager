f7895d6e235de6c66db5fbcc690040dc76c85cc5
ARTYOM MUXAMEDJANOV <artem.mukhamedjanov2007@gmail.com>
2026-06-23 20:46:49 +0500

    added disconnected keyboard and handlers for them

--- bot/handlers/handler.py
+++ bot/handlers/handler.py
@@ -1,58 +1,52 @@
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
-from bot.states.client_states import ClientStates
-from bot.keyboards.inline_keyboards import start_keyboard
+from bot.keyboards.inline_keyboards import start_keyboard, client_keyboard, record_keyboard, contact_keyboard
 
 
-def createRouter() -> Router:
+def create_router() -> Router:
     router = Router()
 
 
     @router.message(F.text == "/start")
     async def start(message: Message):
+        # await message.answer(
+        #     "Отправь номер телефона для регистрации:",
+        #     reply_markup=contact_keyboard())
         await message.answer(f"Здравствуйте {message.from_user.first_name}.\n\n"
                              "Это бот для учета клиентов. С помощью ручного или голосового ввода с ИИ расшифровкой ведется учет клиентской базы.\n\n"
                              'Для ознакомления с функционалом нажмите "Справка".')
-        builder = start_keyboard()
-        await message.answer(text='Выберите вариант: ', reply_markup=builder)
+        await message.answer(text='Выберите вариант: ', reply_markup=start_keyboard())
 
-    @router.message(F.text == "/help")
-    async def help(message: Message):
-        await message.answer("Справочное меню.\n\n\n"
-                             "/client_managing:\n\n"
-                             "  1. Создание нового клиента.\n"
-                             "  2. Удаление клиента.\n"
-                             "  3. Поиск клиента.\n"
-                             "  4. Обновления данных клиента.\n\n\n"
-                             "/record_managing:\n\n"
-                             "1. Создать запись клиента.\n"
-                             "2. Удалить запись клиента.\n"
-                             "3. Поиск записи клиента.\n"
-                             "4. Изменить запись клиента.\n\n\n"
-                             "/start - запуск бота.\n"
-                             "/help - справка.")
+
+#displaying identical help for both command and button
+    @router.message(F.text.in_({"/help", "❓ Справка"}))
+    async def get_help(message: Message):
+        text = ("Справочное меню.\n\n\n"
+                "/client_managing:\n\n"
+                "  1. Создание нового клиента.\n"
+                "  2. Удаление клиента.\n"
+                "  3. Поиск клиента.\n"
+                "  4. Обновления данных клиента.\n\n\n"
+                "/record_managing:\n\n"
+                "1. Создать запись клиента.\n"
+                "2. Удалить запись клиента.\n"
+                "3. Поиск записи клиента.\n"
+                "4. Изменить запись клиента.\n\n\n"
+                "/start - запуск бота.\n"
+                "/help - справка.\n"
+                "/profile - личные данные.")
+        await message.answer(text=text, reply_markup=start_keyboard())
+
+
+
+    @router.message(F.text.in_({"/client_managing", "👤 Управление клиентами"}))
+    async def client_managing(message: Message):
+        await message.answer(text="Выберите действие:" , reply_markup=client_keyboard())
 
     @router.message(Command("create_client"))
-    async def create_client(message: Message, state: FSMContext):
-        await message.answer("Введите Фамилию и Имя:")
-        await state.set_state(ClientStates.client_name)
-
-    @router.message(ClientStates.client_name)
-    async def client_name_handler(message: Message, state: FSMContext):
-        await state.update_data(name=message.text)
-        await message.answer("Введите номер телефона (н.п) 901234567):")
-        await state.set_state(ClientStates.client_phone)
-
-    @router.message(ClientStates.client_phone)
-    async def client_phone_handler(message: Message, state: FSMContext):
-        await state.update_data(phone=message.text)
-        await state.set_state()
-
-    @router.message(ClientStates.client_name, )
-
-
+    async def create_client(message: Message):
+        pass
 
     @router.message(Command("delete_client"))
     async def delete_client(message: Message):
@@ -66,22 +60,35 @@ def createRouter() -> Router:
     async def update_client(message: Message):
         pass
 
-    @router.message(Command("create_record"))
-    async def create_record(message: Message):
-        pass
 
-    @router.message(Command("delete_record"))
-    async def delete_record(message: Message):
-        pass
+    @router.message(F.text.in_({"/record_managing", "📒 Управление записями"}))
+    async def record_managing(message: Message):
+        await message.answer(text="Выберите действие:", reply_markup=record_keyboard())
 
-    @router.message(Command("search_record"))
-    async def search_record(message: Message):
-        pass
+    # @router.message(Command("create_record"))
+    # async def create_record(message: Message):
+    #     pass
+    #
+    # @router.message(Command("delete_record"))
+    # async def delete_record(message: Message):
+    #     pass
+    #
+    # @router.message(Command("search_record"))
+    # async def search_record(message: Message):
+    #     pass
+    #
+    # @router.message(Command("update_record"))
+    # async def update_record(message: Message):
+    #    pass
 
-    @router.message(Command("update_record"))
-    async def update_record(message: Message):
-        pass
 
+    @router.message(F.text.in_({"/profile", "⚙️ Мой профиль"}))
+    async def profile(message: Message):
 
+        await message.answer(f"Профиль\n\n"
+                             f"Имя: {message.from_user.first_name}\n"
+                             f"ID: {message.from_user.id}\n"
+                             f"Username: @{message.from_user.username}"
+                             f"Тип пользователя:")
     return router
 
--- bot/keyboards/inline_keyboards.py
+++ bot/keyboards/inline_keyboards.py
@@ -1,17 +1,27 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 
 def start_keyboard() -> ReplyKeyboardMarkup:
     start_builder = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="👤 Управление клиентами", callback_data="client_managing"),
-    KeyboardButton(text="📒 Управление записями", callback_data="record_managing")],
-    [KeyboardButton(text="❓ Справка", callback_data="help"),
-    KeyboardButton(text="⚙️ Мой профиль", callback_data="profile")]],
+    [KeyboardButton(text="👤 Управление клиентами"),
+    KeyboardButton(text="📒 Управление записями")],
+    [KeyboardButton(text="❓ Справка"),
+    KeyboardButton(text="⚙️ Мой профиль")]],
     resize_keyboard=True, input_field_placeholder='Выберите вариант:')
     return start_builder
 
-def client_managing() -> InlineKeyboardMarkup:
+def contact_keyboard():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📱 Отправить контакт", request_contact=True)]
+        ],
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+
+
+
+def client_keyboard() -> InlineKeyboardMarkup:
     client_builder = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='1. Добавить клиента', callback_data='create_client'),
          InlineKeyboardButton(text='2. Удалить клиента', callback_data='delete_client')],
@@ -21,7 +31,7 @@ def client_managing() -> InlineKeyboardMarkup:
     ], resize_keyboard=True)
     return client_builder
 
-def record_managing() -> InlineKeyboardMarkup:
+def record_keyboard() -> InlineKeyboardMarkup:
     record_builder = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='1. Создать запись', callback_data='create_record'),
          InlineKeyboardButton(text='2. Удалить запись', callback_data='delete_record')],
