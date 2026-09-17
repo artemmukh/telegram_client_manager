8f738c2badf21ae6708c12499d0aa3d725f32a2c
artemmukh <artem.mukhamedjanov2007@gmail.com>
2026-09-17 15:45:04 +0000

    Persist bot logs outside container

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -9,7 +9,7 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from bot.config.config import load_config
 from bot.models.database import Database
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', handlers=[logging.StreamHandler(), logging.FileHandler('/app/logs/bot.log', encoding='utf-8')])
 
 config = load_config()
 
